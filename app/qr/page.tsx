f8cf97e4641383ef3f16f151340c35d73321437f
valeriaSr <valesp_505@hotmail.com>
2024-02-17 01:00:59 -0600

    ..

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,5 +1,5 @@
 import QrCard from '@/app/ui/qr-card';
 
 export default function Page() {
-    return <QrCard imageQr="/image-qr-code.png"/>;
+    return <QrCard imageQr="../image-qr-code.png"/>;
 }
